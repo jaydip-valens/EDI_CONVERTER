9fbad99a8c474a61a4a154c2595e2b24066c3a7a
jaydip-Valens <jaydip.makwana@valensdatalabs.com>
2024-04-15 15:24:38 +0530

    removed all the business logic from the controller, renamed the method name, renamed the response file name, removed unnecessary code and imports and made changes in the services class to remove multiple for-loop

--- CSV/src/main/java/org/example/csv/csv/controller/EdiToCSVController.java
+++ CSV/src/main/java/org/example/csv/csv/controller/EdiToCSVController.java
@@ -1,17 +1,21 @@
 package org.example.csv.csv.controller;
 
-import com.opencsv.CSVWriter;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.example.csv.csv.services.EdiToCSVService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Objects;
 
 @RestController
@@ -20,23 +24,24 @@ public class EdiToCSVController {
     @Autowired
     private EdiToCSVService ediToCSVService;
 
-    @GetMapping("/edi846-to-csv")
-    public Object ediToCsv(@RequestParam(value = "ediFile") MultipartFile ediFile, HttpServletResponse response) {
+    @GetMapping(value = "/edi846-to-csv" , produces = "text/csv")
+    public ResponseEntity<Object> ediToCsv(@RequestParam(value = "ediFile") MultipartFile ediFile, HttpServletResponse response) throws IOException {
+        File responseFile = null;
         try {
-            if (ediFile.isEmpty() || !Objects.equals(FilenameUtils.getExtension(ediFile.getOriginalFilename()), "edi")) {
-                return "invalid File";
+            if (ediFile.isEmpty() && !Objects.equals(FilenameUtils.getExtension(ediFile.getOriginalFilename()), "edi")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid File");
             }
-            Map<String, Object> result = ediToCSVService.fileRead(ediFile);
-            response.setContentType("text/csv");
+            responseFile = ediToCSVService.ediToCSVConvertor(ediFile);
+            byte[] responseFileByte = Files.readAllBytes(responseFile.toPath());
             response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-                    "attachment; filename=\"" + result.get("fileName") + "\"");
-            CSVWriter writer = new CSVWriter(response.getWriter());
-            String[] header = {"Product Name", "Cost", "Quality", "Vendor"};
-            writer.writeNext(header);
-            writer.writeAll((Iterable<String[]>) result.get("csvData"));
+                    "attachment; filename=\"" + responseFile.getName() + "\"");
+            return ResponseEntity.status(HttpStatus.OK).body(responseFileByte);
         } catch (Exception e) {
-            return "Filed To Convert : " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        } finally {
+            if (responseFile != null) {
+                FileUtils.delete(responseFile);
+            }
         }
-        return null;
     }
 }
--- CSV/src/main/java/org/example/csv/csv/services/EdiToCSVService.java
+++ CSV/src/main/java/org/example/csv/csv/services/EdiToCSVService.java
@@ -1,63 +1,62 @@
 package org.example.csv.csv.services;
 
 
+import com.opencsv.CSVWriter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class EdiToCSVService {
 
-    public Map<String, Object> fileRead(MultipartFile ediFile) throws IOException {
+    public File ediToCSVConvertor(MultipartFile ediFile) throws IOException {
+        CSVWriter writer = null;
         try {
-            //        File file = ResourceUtils.getFile("classpath:static/239_GoGoTech_846_163863.edi");
             String content = new String(ediFile.getBytes());
             String[] contentList = content.split("\n");
-            long size = Arrays.stream(contentList).filter(data -> data.startsWith("LIN*")).count();
-            String[][] csvData = new String[(int) size][4];
             String receiverId = "";
             String vendorName = "";
+            String fileName = null;
             int count = 0;
-            for (int i = 0; i < contentList.length; i++) {
-                if (contentList[i].contains("ISA")) {
-                    receiverId = contentList[i].split("\\*")[8].trim();
-                } else if (contentList[i].contains("N1")) {
-                    String[] temp = contentList[i].split("\\*");
+            String[] tempCsvDataArray = new String[4];
+            for (String data : contentList) {
+                if (!receiverId.isBlank() && !vendorName.isBlank() && count == 0) {
+                    fileName = receiverId + "_" + vendorName + ".csv";
+                    writer = new CSVWriter(new FileWriter(fileName));
+                    String[] header = {"Product Name", "Cost", "Quality", "Vendor"};
+                    writer.writeNext(header);
+                }
+                if (data.contains("ISA")) {
+                    receiverId = data.split("\\*")[8].trim();
+                } else if (data.contains("N1")) {
+                    String[] temp = data.split("\\*");
                     vendorName = temp[temp.length - 1].replace(" ", "_").replaceAll("[^a-zA-Z0-9_]", "");
-                } else if (contentList[i].contains("LIN")) {
-                    count = i;
-                    break;
+                } else if (data.contains("LIN")) {
+                    count++;
+                    String[] temp = data.split("\\*");
+                    tempCsvDataArray[3] = temp[temp.length - 1];
+                } else if (data.startsWith("PID*") && data.contains("08")) {
+                    String[] temp = data.split("\\*");
+                    tempCsvDataArray[0] = temp[temp.length - 1].trim();
+                } else if (data.startsWith("CTP*")) {
+                    String[] temp = data.split("\\*");
+                    tempCsvDataArray[1] = temp[temp.length - 1];
+                } else if (data.startsWith("QTY*")) {
+                    String[] temp = data.split("\\*");
+                    tempCsvDataArray[2] = temp[1];
+                    writer.writeNext(tempCsvDataArray);
                 }
             }
-
-            int j = 0;
-            for (int i = count; i < contentList.length; i++) {
-                if (contentList[i].startsWith("LIN*")) {
-                    String[] temp = contentList[i].split("\\*");
-                    csvData[j][3] = temp[temp.length - 1];
-                } else if (contentList[i].startsWith("PID*") && contentList[i].contains("08")) {
-                    String[] temp = contentList[i].split("\\*");
-                    csvData[j][0] = temp[temp.length - 1].trim();
-                } else if (contentList[i].startsWith("CTP*")) {
-                    String[] temp = contentList[i].split("\\*");
-                    csvData[j][1] = temp[temp.length - 1];
-                } else if (contentList[i].startsWith("QTY*")) {
-                    String[] temp = contentList[i].split("\\*");
-                    csvData[j][2] = temp[1];
-                    j++;
-                }
-            }
-            Map<String, Object> resultMap = new HashMap<>();
-            resultMap.put("fileName", vendorName + receiverId + ".csv");
-            resultMap.put("csvData", new ArrayList<>(Arrays.asList(csvData)));
-            return resultMap;
+            return new File(fileName);
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
         }
     }
 }
